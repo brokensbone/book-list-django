64bf859bf26461fab99200576a8bbc43b70a19cf
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-01-27 08:50:17 +0000

    Allow admin editing of models

--- bookapp/reading/models.py
+++ bookapp/reading/models.py
@@ -4,7 +4,14 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
