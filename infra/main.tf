1ba5189f332f448361337f2fcc5e7c996872b63a
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-01-27 21:17:35 +0000

    Basic working version
    Only starts up whoami but its there out the
    box and responding on port 80

--- infra/main.tf
+++ infra/main.tf
@@ -12,24 +12,70 @@ resource "aws_subnet" "subnet-uno" {
 resource "aws_security_group" "ingress-all-test" {
     name = "allow-all-sg"
     vpc_id = "${aws_vpc.test-env.id}"
-    ingress {
-        cidr_blocks = ["0.0.0.0/0"]
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-    }// Terraform removes the default rule
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    # ingress {
+    #     cidr_blocks = ["0.0.0.0/0"]
+    #     from_port = 22
+    #     to_port = 22
+    #     protocol = "tcp"
+    # }// Terraform removes the default rule
+    # egress {
+    #     from_port = 0
+    #     to_port = 0
+    #     protocol = "-1"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
 }
+# resource "aws_vpc_security_group_egress_rule" "egress-all" {
+#   cidr_ipv4   = "10.0.0.0/8"
+#   from_port   = 0
+#   ip_protocol = "tcp"
+#   to_port     = 0
+# }
+resource "aws_security_group_rule" "allow_all_egress" {
+  security_group_id = aws_security_group.ingress-all-test.id
+  type              = "egress"    # 🔄 Outbound traffic
+  from_port         = 0           # 🔑 Any Port
+  to_port           = 65535       # 🔑 Any Port
+  protocol          = "-1"        # 🌐 Any Protocol
+  cidr_blocks       = ["0.0.0.0/0"]  # 🌍 Any IP
+}
+
+# resource "aws_vpc_security_group_ingress_rule" "ingress-ssh" {
+#   security_group_id = aws_security_group.ingress-all-test.id
+#   cidr_ipv4   = "10.0.0.0/8"
+#   from_port   = 22
+#   ip_protocol = "tcp"
+#   to_port     = 22
+# }
+# resource "aws_vpc_security_group_ingress_rule" "ingress-http" {
+#   security_group_id = aws_security_group.ingress-all-test.id
+#   cidr_ipv4   = "10.0.0.0/8"
+#   from_port   = 80
+#   ip_protocol = "tcp"
+#   to_port     = 80
+# }
+resource "aws_security_group_rule" "allow_http" {
+  security_group_id = aws_security_group.ingress-all-test.id
+  type              = "ingress"   # 🔥 Inbound traffic
+  from_port         = 80          # 🔑 Port 80 for HTTP
+  to_port           = 80          # 🔑 Allow to Port 80
+  protocol          = "tcp"       # 📡 TCP Protocol
+  cidr_blocks       = ["0.0.0.0/0"]  # 🌍 Any IP
+}
+resource "aws_security_group_rule" "allow_ssh" {
+  security_group_id = aws_security_group.ingress-all-test.id
+  type              = "ingress"   # 🔥 Inbound traffic
+  from_port         = 22          # 🔑 Port 80 for HTTP
+  to_port           = 22          # 🔑 Allow to Port 80
+  protocol          = "tcp"       # 📡 TCP Protocol
+  cidr_blocks       = ["0.0.0.0/0"]  # 🌍 Any IP
+}
+
 
 resource "aws_instance" "example_server" {
   ami           = "ami-04ba8620fc44e2264"                    
   instance_type = "t2.micro"
-  security_groups = [ "${aws_security_group.ingress-all-test.id}" ]
+  vpc_security_group_ids = [ "${aws_security_group.ingress-all-test.id}" ]
   subnet_id = "${aws_subnet.subnet-uno.id}"
   tags = {
     Name = "MyServer"
@@ -38,6 +84,11 @@ resource "aws_instance" "example_server" {
 #!/bin/bash
 echo "Copying the SSH Key to the server"
 echo -e "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzhdCoWE/CiY3laW9R/I5UEhQs7krz8ur8OOg7su5MJ example@example" >> /home/ec2-user/.ssh/authorized_keys
+yum update -y
+yum install -y docker
+service docker start
+usermod -a -G docker ec2-user
+docker run --rm -d -p 80:80 traefik/whoami
 EOF
 }
 
@@ -61,4 +112,11 @@ resource "aws_route_table" "route-table-test-env" {
 resource "aws_route_table_association" "subnet-association" {
     subnet_id      = "${aws_subnet.subnet-uno.id}"
     route_table_id = "${aws_route_table.route-table-test-env.id}"
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.example_server.public_ip
+}
+output "eip_addr" {
+  value = aws_eip.ip-test-env.public_ip
 }
