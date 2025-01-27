8fdbf8af78ac6c4b7e7b32f78e9509834858a174
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-01-27 08:54:52 +0000

    Very basic listing page

--- bookapp/reading/urls.py
+++ bookapp/reading/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("listing", views.listing, name="listing"),
 ]
--- bookapp/reading/views.py
+++ bookapp/reading/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Book
 
 def index(request):
     return HttpResponse("Hello, world. You're at the library index.")
+
+def listing(request):
+    book_list = Book.objects.order_by("title")[:5]
+    template = loader.get_template("books/listing.html")
+    context = {
+        "book_list": book_list,
+    }
+    return HttpResponse(template.render(context, request))
